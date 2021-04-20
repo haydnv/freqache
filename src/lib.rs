540c9a3c9f873b5aadacf4c31c3e80495d7908d0
Haydn Vestal <haydnv@gmail.com>
2021-04-19 20:26:34 -0500

    implement LFUCache::insert & LFUCache::get

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ struct Item<V> {
     next: Option<RwLock<Self>>,
 }
 
-struct LFUCache<K, V> {
+pub struct LFUCache<K, V> {
     cache: HashMap<K, RwLock<Item<V>>>,
     first: Option<RwLock<Item<V>>>,
     last: Option<RwLock<Item<V>>>,
@@ -50,8 +50,14 @@ impl<K: Eq + Hash, V: Clone> LFUCache<K, V> {
         Q: Hash + Eq,
     {
         if let Some(item) = self.cache.get(key) {
-            if bump(item).await {
-                self.first = Some(item.clone());
+            let (last, first) = bump(item).await;
+
+            if last.is_some() {
+                self.last = last;
+            }
+
+            if first.is_some() {
+                self.first = first;
             }
 
             Some(item.read().map(|lock| lock.value.clone()).await)
@@ -62,8 +68,14 @@ impl<K: Eq + Hash, V: Clone> LFUCache<K, V> {
 
     pub async fn insert(&mut self, key: K, value: V) -> bool {
         if let Some(item) = self.cache.get(&key) {
-            if bump(item).await {
-                self.first = Some(item.clone());
+            let (last, first) = bump(item).await;
+
+            if last.is_some() {
+                self.last = last;
+            }
+
+            if first.is_some() {
+                self.first = first;
             }
 
             true
@@ -77,10 +89,18 @@ impl<K: Eq + Hash, V: Clone> LFUCache<K, V> {
                 next: last,
             });
 
+            if let Some(next) = &item.write().await.next {
+                next.write().await.prev = Some(item.clone());
+            }
+
             self.cache.insert(key, item.clone());
             self.last = Some(item);
             false
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.cache.len()
     }
 
     // TODO: could this return a Stream of &V?
@@ -98,34 +118,96 @@ impl<K: Eq + Hash, V: Clone> LFUCache<K, V> {
     }
 }
 
-async fn bump<V>(item: &RwLock<Item<V>>) -> bool {
+async fn bump<V>(item: &RwLock<Item<V>>) -> (Option<RwLock<Item<V>>>, Option<RwLock<Item<V>>>) {
     let mut item_lock = item.write().await;
 
     if item_lock.next.is_none() {
-        false
+        (None, None) // nothing to update
     } else if item_lock.prev.is_none() && item_lock.next.is_some() {
         let mut next_lock = item_lock.next.as_ref().unwrap().write().await;
 
         mem::swap(&mut next_lock.prev, &mut item_lock.prev); // set next.prev
         mem::swap(&mut item_lock.next, &mut next_lock.next); // set item.next
+        mem::swap(&mut item_lock.prev, &mut next_lock.next); // set item.prev & next.next
 
-        next_lock.next = Some(item.clone()); // set next.next
-        item_lock.prev = Some(item_lock.next.as_ref().unwrap().clone()); // set item.prev
+        let first = if item_lock.next.is_none() {
+            Some(item.clone())
+        } else {
+            None
+        };
 
-        item_lock.next.is_none()
+        (item_lock.prev.clone(), first)
     } else {
         let (mut prev_lock, mut next_lock) = join!(
             item_lock.prev.as_ref().unwrap().write(),
             item_lock.next.as_ref().unwrap().write()
         );
 
+        let next = item_lock.next.clone();
+
         mem::swap(&mut prev_lock.next, &mut item_lock.next); // set prev.next
-        mem::swap(&mut next_lock.prev, &mut item_lock.prev); // set next.prev
         mem::swap(&mut item_lock.next, &mut next_lock.next); // set item.next
+        mem::swap(&mut next_lock.prev, &mut item_lock.prev); // set next.prev
 
-        next_lock.next = Some(item.clone()); // set next.next
-        item_lock.prev = Some(item_lock.next.as_ref().unwrap().clone()); // set item.prev
+        item_lock.prev = next;
 
-        item_lock.next.is_none()
+        let first = if item_lock.next.is_none() {
+            Some(item.clone())
+        } else {
+            None
+        };
+
+        (None, first)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::{thread_rng, Rng};
+
+    #[tokio::test]
+    async fn test_order() {
+        let mut cache = LFUCache::new(100);
+        let values: Vec<i32> = (0..10).collect();
+
+        for i in values.iter().rev() {
+            cache.insert(i, *i).await;
+        }
+
+        assert_eq!(cache.values().await, values)
+    }
+
+    #[tokio::test]
+    async fn test_access() {
+        let mut cache = LFUCache::new(100);
+
+        let mut rng = thread_rng();
+        for _ in 0..100_000 {
+            let i: i32 = rng.gen_range(10..20);
+            println!("insert {}", i);
+            cache.insert(i, i).await;
+
+            let mut next = cache.last.clone();
+            while let Some(item) = next {
+                let lock = item.read().await;
+
+                if let Some(item) = lock.prev.as_ref() {
+                    print!("{}-", item.read().await.value);
+                }
+
+                print!("{}", lock.value);
+
+                next = lock.next.clone();
+                if let Some(item) = &next {
+                    print!("-{}", item.read().await.value);
+                }
+
+                print!(" ");
+            }
+
+            println!();
+            assert_eq!(cache.len(), cache.values().await.len());
+        }
     }
 }
