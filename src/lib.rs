8a52c91c62c088170a230dd2451bc419d907c5f9
Haydn Vestal <haydnv@gmail.com>
2021-04-19 21:45:29 -0500

    implement LFUCache::remove

--- src/lib.rs
+++ src/lib.rs
@@ -102,6 +102,43 @@ impl<K: Eq + Hash, V: Entry> LFUCache<K, V> {
         self.cache.len()
     }
 
+    pub async fn remove<Q>(&mut self, key: &Q) -> bool
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+        if let Some(item) = self.cache.remove(key) {
+            let mut item_lock = item.write().await;
+
+            if item_lock.prev.is_none() && item_lock.next.is_none() {
+                self.last = None;
+                self.first = None;
+            } else if item_lock.prev.is_none() {
+                self.last = item_lock.next.clone();
+
+                let mut next = item_lock.next.as_ref().unwrap().write().await;
+                mem::swap(&mut next.prev, &mut item_lock.prev);
+            } else if item_lock.next.is_none() {
+                self.first = item_lock.prev.clone();
+
+                let mut prev = item_lock.prev.as_ref().unwrap().write().await;
+                mem::swap(&mut prev.next, &mut item_lock.next);
+            } else {
+                let (mut prev, mut next) = join!(
+                    item_lock.prev.as_ref().unwrap().write(),
+                    item_lock.next.as_ref().unwrap().write()
+                );
+
+                mem::swap(&mut next.prev, &mut item_lock.prev);
+                mem::swap(&mut prev.next, &mut item_lock.next);
+            }
+
+            true
+        } else {
+            false
+        }
+    }
+
     pub async fn traverse<F: FnMut(&V) -> () + Send>(&self, mut f: F) {
         let mut next = self.last.clone();
         while let Some(item) = next {
@@ -115,8 +152,8 @@ impl<K: Eq + Hash, V: Entry> LFUCache<K, V> {
 async fn bump<V>(item: &RwLock<Item<V>>) -> (Option<RwLock<Item<V>>>, Option<RwLock<Item<V>>>) {
     let mut item_lock = item.write().await;
 
-    if item_lock.next.is_none() {
-        (None, None) // nothing to update
+    let last = if item_lock.next.is_none() {
+        return (None, None); // nothing to update
     } else if item_lock.prev.is_none() && item_lock.next.is_some() {
         let mut next_lock = item_lock.next.as_ref().unwrap().write().await;
 
@@ -124,13 +161,7 @@ async fn bump<V>(item: &RwLock<Item<V>>) -> (Option<RwLock<Item<V>>>, Option<RwL
         mem::swap(&mut item_lock.next, &mut next_lock.next); // set item.next
         mem::swap(&mut item_lock.prev, &mut next_lock.next); // set item.prev & next.next
 
-        let first = if item_lock.next.is_none() {
-            Some(item.clone())
-        } else {
-            None
-        };
-
-        (item_lock.prev.clone(), first)
+        item_lock.prev.clone()
     } else {
         let (mut prev_lock, mut next_lock) = join!(
             item_lock.prev.as_ref().unwrap().write(),
@@ -145,21 +176,64 @@ async fn bump<V>(item: &RwLock<Item<V>>) -> (Option<RwLock<Item<V>>>, Option<RwL
 
         item_lock.prev = next; // set item.prev
 
-        let first = if item_lock.next.is_none() {
-            Some(item.clone())
-        } else {
-            None
-        };
+        None
+    };
 
-        (None, first)
-    }
+    let first = if item_lock.next.is_some() {
+        let mut skip_lock = item_lock.next.as_ref().unwrap().write().await;
+        skip_lock.prev = Some(item.clone());
+        None
+    } else {
+        Some(item.clone())
+    };
+
+    (last, first)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fmt;
+
     use rand::{thread_rng, Rng};
 
     use super::*;
+
+    async fn print_debug<K, V: fmt::Display>(cache: &LFUCache<K, V>) {
+        let mut next = cache.last.clone();
+        while let Some(item) = next {
+            let lock = item.read().await;
+
+            if let Some(item) = lock.prev.as_ref() {
+                print!("{}-", item.read().await.value);
+            }
+
+            print!("{}", lock.value);
+
+            next = lock.next.clone();
+            if let Some(item) = &next {
+                print!("-{}", item.read().await.value);
+            }
+
+            print!(" ");
+        }
+
+        println!();
+    }
+
+    async fn validate<K, V: Copy + Eq + fmt::Debug>(cache: &LFUCache<K, V>) {
+        let mut last = None;
+        let mut next = cache.last.clone();
+        while let Some(item) = next {
+            let lock = item.read().await;
+
+            if let Some(last) = last {
+                assert_eq!(lock.prev.as_ref().unwrap().read().await.value, last);
+            }
+
+            last = Some(lock.value);
+            next = lock.next.clone();
+        }
+    }
 
     #[async_trait]
     impl Entry for i32 {
@@ -188,29 +262,25 @@ mod tests {
 
         let mut rng = thread_rng();
         for _ in 0..100_000 {
-            let i: i32 = rng.gen_range(10..20);
+            let i: i32 = rng.gen_range(0..10);
             println!("insert {}", i);
             cache.insert(i, i).await;
 
-            let mut next = cache.last.clone();
-            while let Some(item) = next {
-                let lock = item.read().await;
+            print_debug(&cache).await;
+            validate(&cache).await;
 
-                if let Some(item) = lock.prev.as_ref() {
-                    print!("{}-", item.read().await.value);
-                }
+            let mut size = 0;
+            cache.traverse(|_| size += 1).await;
+            assert_eq!(cache.len(), size);
 
-                print!("{}", lock.value);
+            let i: i32 = rng.gen_range(0..10);
+            println!("remove {}", i);
+            cache.remove(&i).await;
 
-                next = lock.next.clone();
-                if let Some(item) = &next {
-                    print!("-{}", item.read().await.value);
-                }
+            print_debug(&cache).await;
+            validate(&cache).await;
+            assert!(!cache.contains_key(&i));
 
-                print!(" ");
-            }
-
-            println!();
             let mut size = 0;
             cache.traverse(|_| size += 1).await;
             assert_eq!(cache.len(), size);
